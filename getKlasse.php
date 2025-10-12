90a8a29044fddee9a4fa9ba0f449b3267038736a
RobertBkB <robert.mueller@berufskolleg-borken.de>
2025-10-12 18:20:51 +0200

    Add files via upload

---
+++ getKlasse.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-Type: application/json");
+
+// Sicherheitsmaßnahme: Nur GET mit Parameter "id" erlaubt
+if (!isset($_GET['id'])) {
+    echo json_encode(["error" => "Keine ID übergeben"]);
+    exit;
+}
+
+$id = $_GET['id'];
+$rezepte = json_decode(file_get_contents("rezepte.json"), true);
+
+foreach ($rezepte as $klasse => $ids) {
+    if (in_array($id, $ids)) {
+        echo json_encode(["klasse" => $klasse]);
+        exit;
+    }
+}
+
+echo json_encode(["error" => "ID nicht gefunden"]);
+?>
